27ef42eafbc2a21a872ee07f7d594f83bb6b2b12
Krishna Goli <krishna.goli@ladbrokes.co.uk>
2017-12-03 20:09:21 +0000

    first upload

---
+++ Colt Bootcamp/javascript.js
@@ -0,0 +1,17 @@
+var age = Number(prompt("What is your age?"))
+
+if (age<0) {
+	console.log("Come back once you are out of the womb")
+}
+
+if (age===21) {
+	console.log("Happy 21st Birthday")
+}
+
+if (age % 2 !==0) {
+	console.log("Your age is odd")
+}
+
+if (age % Math.sqrt(age) ===0) {
+	console.log("Your age is a perfect square")
+}
---
+++ Colt Bootcamp/while.js
@@ -0,0 +1,7 @@
+// Print All Numbers between -10 and 19
+
+// Print all even numbers between 10 and 40
+
+// Print all Odd numbers between 300 and 333
+
+// Print all numbers divisible by 5 and 3 between 5 and 50
