aa8aca9d01decdf80098659f56da60dcaa272445
krishnagoli <34218824+krishnagoli@users.noreply.github.com>
2017-12-03 21:05:52 +0000

    while complete

--- Colt Bootcamp/while.js
+++ Colt Bootcamp/while.js
@@ -1,19 +1,42 @@
-// Print All Numbers between -10 and 19
-var value = "-10";
+alert
 
-while value <= 19 {
-	console.log(Print(value));
+// Print All Numbers between -10 and 19
+console.log("Print All Numbers between -10 and 19")
+var counter = -10;
+
+while(counter <= 19) {
+	console.log(counter);
+	counter++;
 }
 
 // Print all even numbers between 10 and 40
-
-var value = "-10";
-
-while value <= 19 {
-	console.log(Print(value));
+console.log("Print all even numbers between 10 and 40")
+var counter = 10;
+while(counter <= 40) {
+	if(counter % 2 === 0){
+	console.log(counter);
+	}
+	counter+=1;
 }
 
 
 // Print all Odd numbers between 300 and 333
+console.log("Print all Odd numbers between 300 and 333")
+var counter = 300;
+while(counter <= 333) {
+	if (counter % 2 == 1) {
+	console.log(counter);
+	}
+	counter+=1;
+}
+
 
 // Print all numbers divisible by 5 and 3 between 5 and 50
+console.log("Print all numbers divisible by 5 and 3 between 5 and 50")
+var counter = 5;
+while(counter <= 50) {
+	if (counter % 5 === 0 && counter % 3 === 0) {
+	console.log(counter);
+	}
+	counter+=1;
+}
