bfd96e67079e6ba28fb06a94d78d7a8949608f74
krishnagoli <34218824+krishnagoli@users.noreply.github.com>
2017-12-03 20:37:41 +0000

    3rd commit

--- Colt Bootcamp/while.js
+++ Colt Bootcamp/while.js
@@ -7,6 +7,13 @@ while value <= 19 {
 
 // Print all even numbers between 10 and 40
 
+var value = "-10";
+
+while value <= 19 {
+	console.log(Print(value));
+}
+
+
 // Print all Odd numbers between 300 and 333
 
 // Print all numbers divisible by 5 and 3 between 5 and 50
